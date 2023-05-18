5adeb4dd425e2dc9fb22b5a66ddcf9f80e8cc728
angelinaustala <angelinaazaran11@gmail.com>
2023-05-18 23:22:46 +0300

    7lab

--- ффф.py
+++ ффф.py
@@ -1,16 +1,1 @@
 
-
-from PIL import Image, ImageFilter
-def ypr1():
-
- image = Image.open('
- width, height = image.size
- forma = image.format
- mode = image.mode
-
- print("ширина", width)
- print("высота", height)
- print("формат", forma)
- print("цветовая модель", mode)
-
-ypr1()
